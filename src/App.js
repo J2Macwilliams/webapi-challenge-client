cfa21e984f170a4c49032328f31574f4c1c6110d
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-06 17:02:01 -0800

    make get call to api. setPerson with response. .map through person and display projects. Style cards.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ProjectsList from './components/ProjectsList'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ProjectsList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ProjectsList.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import { Paper, Grid, Container, Card, Typography, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: 5,
+        background: '#111d5e'
+    },
+    card: {
+        margin: 10,
+        padding: 10,
+        background: '#b21f66',
+        color: ''
+    }
+}));
+
+function ProjectsList() {
+    const [projects, setProjects] = useState([])
+    const classes = useStyles();
+
+    useEffect(() => {
+        axios.get('https://sprendpoint1.herokuapp.com/api/projects/')
+            .then(res => {
+                console.log(res)
+                setProjects(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+    }, [])
+    return (
+        <div>
+            <Container className={classes.root}>
+                <Paper className={classes.paper}>
+                    <Typography variant="h3">Projects</Typography>
+                </Paper>
+                <Grid container spacing={1}>
+                    {projects.map((emily , id) => (
+                        <Grid item key={id} xs={12} sm={6} md={3}>
+                            <Card className={classes.card}>
+                                <Typography variant="h3">
+                                    {emily.name}
+                                </Typography>
+                                <Typography variant="h4">
+                                    {emily.description}
+                                </Typography>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+        </div>
+    )
+}
+
+export default ProjectsList
